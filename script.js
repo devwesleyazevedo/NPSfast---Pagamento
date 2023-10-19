c4450a09d179011445683de5d03405dd6db7f877
devwesleyazevedo <dev.wesleyazevedo@gmail.com>
2023-10-19 15:41:35 -0300

    First commit

---
+++ script.js
@@ -0,0 +1,263 @@
+/*
+See on github: https://github.com/muhammederdem/credit-card-form
+*/
+
+new Vue({
+  el: "#app",
+  data() {
+    return {
+      currentCardBackground: Math.floor(Math.random()* 25 + 1), // just for fun :D
+      cardName: "",
+      cardNumber: "",
+      cardMonth: "",
+      cardYear: "",
+      cardCvv: "",
+      minCardYear: new Date().getFullYear(),
+      amexCardMask: "#### ###### #####",
+      otherCardMask: "#### #### #### ####",
+      cardNumberTemp: "",
+      isCardFlipped: false,
+      focusElementStyle: null,
+      isInputFocused: false
+    };
+  },
+  mounted() {
+    this.cardNumberTemp = this.otherCardMask;
+    document.getElementById("cardNumber").focus();
+  },
+  computed: {
+    getCardType () {
+      let number = this.cardNumber;
+      let re = new RegExp("^4");
+      if (number.match(re) != null) return "visa";
+
+      re = new RegExp("^(34|37)");
+      if (number.match(re) != null) return "amex";
+
+      re = new RegExp("^5[1-5]");
+      if (number.match(re) != null) return "mastercard";
+
+      re = new RegExp("^6011");
+      if (number.match(re) != null) return "discover";
+      
+      re = new RegExp('^9792')
+      if (number.match(re) != null) return 'troy'
+
+      return "visa"; // default type
+    },
+		generateCardNumberMask () {
+			return this.getCardType === "amex" ? this.amexCardMask : this.otherCardMask;
+    },
+    minCardMonth () {
+      if (this.cardYear === this.minCardYear) return new Date().getMonth() + 1;
+      return 1;
+    }
+  },
+  watch: {
+    cardYear () {
+      if (this.cardMonth < this.minCardMonth) {
+        this.cardMonth = "";
+      }
+    }
+  },
+  methods: {
+    flipCard (status) {
+      this.isCardFlipped = status;
+    },
+    focusInput (e) {
+      this.isInputFocused = true;
+      let targetRef = e.target.dataset.ref;
+      let target = this.$refs[targetRef];
+      this.focusElementStyle = {
+        width: `${target.offsetWidth}px`,
+        height: `${target.offsetHeight}px`,
+        transform: `translateX(${target.offsetLeft}px) translateY(${target.offsetTop}px)`
+      }
+    },
+    blurInput() {
+      let vm = this;
+      setTimeout(() => {
+        if (!vm.isInputFocused) {
+          vm.focusElementStyle = null;
+        }
+      }, 300);
+      vm.isInputFocused = false;
+    }
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var next_click=document.querySelectorAll(".next_button");
+var main_form=document.querySelectorAll(".main");
+var step_list = document.querySelectorAll(".progress-bar li");
+var num = document.querySelector(".step-number");
+let formnumber=0;
+
+next_click.forEach(function(next_click_form){
+    next_click_form.addEventListener('click',function(){
+        if(!validateform()){
+            return false
+        }
+       formnumber++;
+       updateform();
+       progress_forward();
+       contentchange();
+    });
+}); 
+
+var back_click=document.querySelectorAll(".back_button");
+back_click.forEach(function(back_click_form){
+    back_click_form.addEventListener('click',function(){
+       formnumber--;
+       updateform();
+       progress_backward();
+       contentchange();
+    });
+});
+
+var username=document.querySelector("#user_name");
+var shownname=document.querySelector(".shown_name");
+ 
+
+var submit_click=document.querySelectorAll(".submit_button");
+submit_click.forEach(function(submit_click_form){
+    submit_click_form.addEventListener('click',function(){
+       shownname.innerHTML= username.value;
+       formnumber++;
+       updateform(); 
+    });
+});
+
+var heart=document.querySelector(".fa-heart");
+heart.addEventListener('click',function(){
+   heart.classList.toggle('heart');
+});
+
+
+var share=document.querySelector(".fa-share-alt");
+share.addEventListener('click',function(){
+   share.classList.toggle('share');
+});
+
+ 
+
+function updateform(){
+    main_form.forEach(function(mainform_number){
+        mainform_number.classList.remove('active');
+    })
+    main_form[formnumber].classList.add('active');
+} 
+ 
+function progress_forward(){
+    // step_list.forEach(list => {
+        
+    //     list.classList.remove('active');
+         
+    // }); 
+    
+     
+    num.innerHTML = formnumber+1;
+    step_list[formnumber].classList.add('active');
+}  
+
+function progress_backward(){
+    var form_num = formnumber+1;
+    step_list[form_num].classList.remove('active');
+    num.innerHTML = form_num;
+} 
+ 
+var step_num_content=document.querySelectorAll(".step-number-content");
+
+ function contentchange(){
+     step_num_content.forEach(function(content){
+        content.classList.remove('active'); 
+        content.classList.add('d-none');
+     }); 
+     step_num_content[formnumber].classList.add('active');
+ } 
+ 
+ 
+function validateform(){
+    validate=true;
+    var validate_inputs=document.querySelectorAll(".main.active input");
+    validate_inputs.forEach(function(vaildate_input){
+        vaildate_input.classList.remove('warning');
+        if(vaildate_input.hasAttribute('require')){
+            if(vaildate_input.value.length==0){
+                validate=false;
+                vaildate_input.classList.add('warning');
+            }
+        }
+    });
+    return validate;
+    
+}
+
+
+
+
+
+
+
+
+function toggleCampos() {
+  var selectElement = document.getElementById("tipoCliente");
+  var divJuridica = document.getElementById("juridica");
+
+  if (selectElement.value === "Pessoa Física") {
+      // Se a opção Pessoa Física for selecionada, oculte a div "juridica".
+      divJuridica.style.display = "none";
+  } else if (selectElement.value === "Pessoa Jurídica") {
+      // Se a opção Pessoa Jurídica for selecionada, mostre a div "juridica".
+      divJuridica.style.display = "flex"; // Ou "inline" ou qualquer outro valor de exibição apropriado.
+  }
+}
+
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+                      // Obtenha referências aos elementos HTML
+                     const checkbox = document.getElementById("input_cov");
+                     const divEndCob = document.getElementById("end_cob");
+
+                     // Adicione um ouvinte de evento ao checkbox
+                     checkbox.addEventListener("change", function() {
+                     if (checkbox.checked) {
+                        // Se o checkbox estiver marcado, oculte a div
+                        divEndCob.style.display = "none";
+                     } else {
+                        // Se o checkbox não estiver marcado, mostre a div
+                        divEndCob.style.display = "block";
+                     }
+                     });
+});
